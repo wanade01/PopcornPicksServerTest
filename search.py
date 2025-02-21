39d9dab9efec2c33d7a5e231f87dbd614444a0ff
wanade01 <wanade01@gmail.com>
2025-02-20 21:09:14 -0800

    Update search.py added functions to run function

--- search.py
+++ search.py
@@ -190,4 +190,5 @@ def testServer():
     return "Hello World!"
 
 if __name__ == '__main__':
-    search.run()
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no PORT variable is set
+    search.run(host="0.0.0.0", port=port, debug=True)
