7cf008af6eec4f4a680ca05805ff8ad492b6877c
wanade01 <wanade01@gmail.com>
2025-02-20 19:20:56 -0800

    Update app.py to fix CORS issue

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sqlalchemy.sql.expression import func
 
 app = Flask(__name__)
 app.debug = True
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*.github.io"}})
 
 #adds config for using a MySQL DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:5PTjjJBDcpLt4PFuacku@popcornpicks.czkyuwkeqdlw.us-west-1.rds.amazonaws.com:3306/popcornpicksdb'
